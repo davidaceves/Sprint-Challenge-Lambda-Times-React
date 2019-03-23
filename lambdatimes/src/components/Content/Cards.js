a8302cddbf33ad2b282595c207bac237813417f3
David <digital0david@gmail.com>
2019-03-23 14:51:11 -0400

    proptypes

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Card from './Card';
 
+import PropTypes from "prop-types";
+
 const Cards = props => {
   return (
     <div className="cards-container">
@@ -14,5 +16,16 @@ const Cards = props => {
 }
 
 // Make sure you include prop types for all of your incoming props
+
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(
+      PropTypes.shape({
+        headline: PropTypes.string,
+        tab: PropTypes.string,
+        img: PropTypes.string,
+        author: PropTypes.string 
+      })
+  )
+}
 
 export default Cards;
