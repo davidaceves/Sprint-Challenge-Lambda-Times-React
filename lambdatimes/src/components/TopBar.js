fd9646a4a92b8111b077047eadc0b2b44453d024
David <digital0david@gmail.com>
2019-03-23 14:57:00 -0400

    failed to add styled css

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import styled from "styled-components";
+
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+// const WrapperTopBar = styled.div`
+//   width: 100%;
+//   display: flex;
+//   justify-content: center;
+//   align-items: none;
+//   flex-direction: row;
+//   position: fixed;
+//   height: 44px;
+//   background-color: #333;
+// `;
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
+    <div className='top-bar'>
       <div className="container">
         <div className="container-left">
           <span>TOPICS</span><span>SEARCH</span>
